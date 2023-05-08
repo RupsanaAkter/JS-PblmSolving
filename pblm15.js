7c122e935c349f63c8347a590f4d05c015816b6b
R7558 <rupsanaworld1004@gmail.co>
2023-05-08 13:41:56 +0600

    added pblm15

---
+++ pblm15.js
@@ -0,0 +1,28 @@
+// fizz buzz Input: n = 3
+// Output: ["1","2","Fizz"] Input: n = 5
+// Output: ["1","2","Fizz","4","Buzz"] Input: n = 15
+// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"] 
+
+function fizzBuzz(n) {
+    const result = [];
+  
+    for (let i = 1; i <= n; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        result.push("FizzBuzz");
+      } else if (i % 3 === 0) {
+        result.push("Fizz");
+      } else if (i % 5 === 0) {
+        result.push("Buzz");
+      } else {
+        result.push(i.toString());
+      }
+    }
+  
+    return result;
+  }
+  
+  // Test the function
+  console.log(fizzBuzz(3)); // Output: ["1", "2", "Fizz"]
+  console.log(fizzBuzz(5)); // Output: ["1", "2", "Fizz", "4", "Buzz"]
+  console.log(fizzBuzz(15)); // Output: ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
+  
